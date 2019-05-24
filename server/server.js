7875439a99c23f0a3d8ec7753b4a6b204c3248ea
tblais1224 <thomasblais.student@careerdevs.com>
2019-05-24 14:42:52 -0400

    connected db

--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const graphqlHTTP = require("express-graphql")
 const schema = require("./schema/schema")
+const mongoURI = require("./config/keys").mongoURI
 
 const app = express();
+
+//connect to mongo atlas
+mongoose.connect(mongoURI, { useNewUrlParser: true })
+mongoose.connection.once("open", () => {
+    console.log("MongoDB Connected")
+})
 
 //graphql middleware to handle gql reqs
 app.use("/graphql", graphqlHTTP({
     schema,
     graphiql: true
 }))
-
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on port: " + PORT));
