bd579d114a49124dbd3008ceacf482583afac1c4
tblais1224 <thomasblais.student@careerdevs.com>
2019-05-26 00:14:17 -0400

    add gamelist compo

---
+++ client/src/components/GameList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class GameList extends Component {
+    render() {
+        return (
+            <div>
+                <ul id="game-list">
+                    <li>Game Name</li>
+                </ul>
+            </div>
+        )
+    }
+}
+export default GameList
